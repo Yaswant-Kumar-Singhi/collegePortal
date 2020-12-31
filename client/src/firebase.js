2c044db1d11ac4ff35ae9504ce18e88602b9490f
YASWANT KUMAR SINGHI <40240984+Yaswant-Kumar-Singhi@users.noreply.github.com>
2020-12-31 15:18:15 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyDVnuzwd7P0eZOGOPJyJmzZyniUg1HHcQI",
-    authDomain: "collegeportal-8abc7.firebaseapp.com",
-    projectId: "collegeportal-8abc7",
-    storageBucket: "collegeportal-8abc7.appspot.com",
-    messagingSenderId: "901511078104",
-    appId: "1:901511078104:web:386c06abc3d5a4f21f77ad"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 
 })
 
